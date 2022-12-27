064fa8a6dfccf1778b6dfb49adcade1f39920ab7
dcLima7 <118079060+dcLima7@users.noreply.github.com>
2022-12-27 11:15:06 -0300

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+const dia = document.getElementById("dia")
+const hora = document.getElementById("hora")
+const minuto = document.getElementById("minuto")
+const segundo = document.getElementById("segundo")
+
+const lancamento = "17 jan 2023"
+
+function countDown(){
+
+    const dataLancamento = new Date(lancamento)
+    const hoje = new Date()
+
+    const segundosTotal = (dataLancamento - hoje)/1000;
+
+    const finalDias = Math.floor(segundosTotal / 60 / 60 / 24);
+    const finalHoras = Math.floor(segundosTotal / 60 / 60) % 24;
+    const finalMinutos = Math.floor(segundosTotal / 60) % 60;
+    const finalSegundos = Math.floor(segundosTotal) % 60;
+
+    dia.innerHTML = finalDias
+    hora.innerHTML = formatoTempo (finalHoras)
+    minuto.innerHTML= formatoTempo (finalMinutos)
+    segundo.innerHTML = formatoTempo (finalSegundos)
+}
+
+function formatoTempo( tempo ){
+    return tempo <10? `0${tempo}` : tempo;
+}
+
+countDown();
+setInterval(countDown, 1000)
